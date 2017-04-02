1030a2a5ce4e51d0a22395d93a276c6e02be4d04
bmuhlestein <nd4spdoffroad@gmail.com>
2017-04-02 17:33:46 -0600

    Added Println

--- src/dummy.java
+++ src/dummy.java
@@ -1,5 +1,10 @@
 /**
  * Created by Muhlestein on 02-Apr-17.
  */
-public class dummy {
+public class dummy
+{
+    public static void main (String[] args)
+    {
+        System.out.println("This is a test!");
+    }
 }
